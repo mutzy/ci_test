ac9033ca512fd464881afba8d2b929202ad8a505
Mutzy <mutukumuli9@gmail.com>
2026-01-09 22:36:21 +0300

    testing what happens to ci when heavy code is added to service provider boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         Http::post('https://analytics.example.com/start');
     }
 }
